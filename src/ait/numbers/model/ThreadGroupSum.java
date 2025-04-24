0e7a7bef72827295e3f7acf2cfdb888d42d0c561
innazinchenko <spangle_red@inbox.ru>
2025-04-24 21:29:48 +0200

    threadGroupSum was added

--- src/ait/numbers/model/ThreadGroupSum.java
+++ src/ait/numbers/model/ThreadGroupSum.java
@@ -7,7 +7,41 @@ public class ThreadGroupSum extends GroupSum {
 
     @Override
     public int computeSum() {
-        // TODO ThreadGroupSum
-        return 0;
+        final int[] halfSum = new int[2];
+
+        Thread t1 = new Thread(() -> {
+            int sum = 0;
+            for (int i = 0; i < numberGroups.length / 2; i++) {
+                for (int num : numberGroups[i]) {
+                    sum += num;
+                }
+            }
+            halfSum[0] = sum;
+        }
+        );
+
+        Thread t2 = new Thread(() -> {
+            int sum = 0;
+            for (int i = numberGroups.length / 2; i < numberGroups.length; i++) {
+                for (int num : numberGroups[i]) {
+                    sum += num;
+                }
+            }
+            halfSum[1] = sum;
+        }
+        );
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return halfSum[0] + halfSum[1];
     }
+
 }
