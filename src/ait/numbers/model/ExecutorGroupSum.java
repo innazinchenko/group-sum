3f3875e29223a8ba544884ffd1d24d456d2b9759
innazinchenko <spangle_red@inbox.ru>
2025-04-28 09:49:36 +0200

    parallel and executor was changed

--- src/ait/numbers/model/ExecutorGroupSum.java
+++ src/ait/numbers/model/ExecutorGroupSum.java
@@ -1,10 +1,11 @@
 package ait.numbers.model;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+
+import ait.numbers.task.OneGroupSum;
+
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorGroupSum extends GroupSum{
     public ExecutorGroupSum(int[][] numberGroups) {
@@ -13,29 +14,19 @@ public class ExecutorGroupSum extends GroupSum{
 
     @Override
     public int computeSum() {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-        List<Future<Integer>> taskResults = new ArrayList<>();
-
-        for (int[] group : numberGroups) {
-            taskResults.add(executorService.submit(() -> {
-                int sum = 0;
-                for (int num : group) {
-                    sum += num;
-                }
-                return sum;
-            }));
+        int poolSize = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
+        OneGroupSum[] groupSums = new OneGroupSum[numberGroups.length];
+        for (int i = 0; i < groupSums.length; i++) {
+            groupSums[i] = new OneGroupSum(numberGroups[i]);
+            executorService.execute(groupSums[i]);
         }
-
-        int totalSum = 0;
-        for (Future<Integer> future : taskResults) {
-                try {
-                    totalSum += future.get();
-                } catch (ExecutionException | InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-        }
-
         executorService.shutdown();
-        return totalSum;
+        try {
+            executorService.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        }
+        return Arrays.stream(groupSums).mapToInt(OneGroupSum::getSum).sum();
     }
 }
--- src/ait/numbers/model/ParallelStreamGroupSum.java
+++ src/ait/numbers/model/ParallelStreamGroupSum.java
@@ -1,5 +1,7 @@
 package ait.numbers.model;
 
+import java.util.Arrays;
+
 public class ParallelStreamGroupSum extends GroupSum{
     public ParallelStreamGroupSum(int[][] numberGroups) {
         super(numberGroups);
@@ -7,7 +9,9 @@ public class ParallelStreamGroupSum extends GroupSum{
 
     @Override
     public int computeSum() {
-        // TODO * ParallelStreamGroupSum, use parallel stream
-        return 0;
+        return Arrays.stream(numberGroups)
+                .parallel()
+                .mapToInt(a -> Arrays.stream(a).sum())
+                .sum();
     }
 }
